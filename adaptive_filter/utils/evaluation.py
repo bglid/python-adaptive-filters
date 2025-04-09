07074986dd69738e40016499ab76d7b96c7743e9
bglid <benjamin.h.glidden@gmail.com>
2025-04-09 10:33:30 -0400

    Updating evaluation to include LMS convergence

--- adaptive_filter/utils/evaluation.py
+++ adaptive_filter/utils/evaluation.py
@@ -2,9 +2,12 @@
 EVALUATION METRICS: MSE, SNR, Convergence (rate and time), Misadjustment, Clock time (for algorithm complexity)
 """
 
+import collections
 import time
 
 import numpy as np
+
+from adaptive_filter.utils.math_utils import EigenDecomposition
 
 
 class EvaluationSuite:
@@ -44,16 +47,43 @@ class EvaluationSuite:
         return 10 * np.log10(snr)
 
     # function for Convergance
-    def convergence_rate(
+    def lms_convergence_rate(
         self,
         step_size,
+        input_signal,
+        k,
     ):
-        """Calculates the convergance rate dependent on mu and autocorrelation eigenvalue"""
-        pass
+        """Calculates the first-order convergance rate of the mean-square error dependent on mu and autocorrelation eigenvalue.
+        This process is done by first taking the autocorrelation (R) of the input data, X^K, where K indicates time.
+        Then, eigendecomposition is run to get the the min eigenvalue, indicating the slowet possible decay,
+        which is used in the final calc. (1 - 2 * step_size * lambda_i)^K
+        """
+        # first we get the autocorrelation matrix R = X^{k}
+        autocorrelation = np.dot(input_signal, input_signal.T)
+        # Running Eigendecomposition on the auto correlation
+        eigen = EigenDecomposition(k_iterations=25)
+        W, lambda_matrix, eigenvalues = eigen.eigendecomposition(
+            covariance_matrix=autocorrelation, n_eigenvectors=10
+        )
+        # getting the smallest eigenvalue for our convergence rate after K time
+        max_lambda = np.min(eigenvalues)
+        return np.abs(1 - 2 * step_size * max_lambda) ** k
 
     # function to run full evaulation/benchmark suite
-    def evaluation(self, desired_signal, output_signal):
-        pass
+    def evaluation(self, desired_signal, input_signal, step_size, time_k):
+        """Calculates and returns suite of evaluation metrics
+
+        Args:
+            desired_signal (float): Input Signal
+            input_signal (float): Input Noise
+
+        Returns:
+            dict: Dictionary of evaluation results
+        """
+
+        eval_results = collections.defaultdict(float)
+
+        eval_results["MSE"] = self.MSE(desired_signal, input_signal)
 
 
 if __name__ == "__main__":
