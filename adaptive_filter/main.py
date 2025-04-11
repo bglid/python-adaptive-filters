1cdafd0edaf9436d87705c2f9707e74bac5a4a8d
bglid <benjamin.h.glidden@gmail.com>
2025-04-11 08:06:00 -0400

    Updating plotting file

--- adaptive_filter/main.py
+++ adaptive_filter/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python4
 """
 Main entry point for Adaptive Filter protoyping
 """
@@ -17,37 +17,48 @@ def main(clean_signal, noisy_signal, args):
 
     # setting up filter
     # mu = args.mu
-    mu = 0.45
+    mu = 0.44
     lms_af = LMS(mu=mu, n=16)
     y, error, results = lms_af.filter(
         d=clean_signal, x=noisy_signal, eval_at_sample=5000
     )
 
     # quick plotting
-    # print(len(results["MSE"]))
-    plt.figure(figsize=(10, 6))
-    x_axis = np.arange(0, len(results["MSE"]))
-    plt.plot(x_axis, results["MSE"], c="darkviolet")
-    plt.title("MSE")
-    plt.grid()
+    plots = plotting.PlotSuite(algorithm=lms_af.algorithm, num_samples=len(y))
+    plt.subplot(3, 1, 1)
+    clean_plot = plots.signal_plot(
+        clean_signal[-2000:], description="Clean signal input", subplot=True
+    )
+
+    plt.subplot(3, 1, 2)
+    noisy_plot = plots.signal_plot(
+        noisy_signal[-2000:], description="Noisy signal input", subplot=True
+    )
+
+    plt.subplot(3, 1, 3)
+    output_plot = plots.signal_plot(
+        y[-2000:],
+        description=f"Noisy signal output, with step size = {mu}",
+        subplot=True,
+    )
+    plt.tight_layout()
+    plt.savefig(f"./data/result_images/{lms_af.algorithm}_results.pdf")
     plt.show()
 
-    plt.figure(figsize=(10, 6))
-    x_axis = np.arange(0, len(results["SNR"]))
-    plt.plot(x_axis, results["SNR"], c="crimson")
-    plt.title("SNR")
-    plt.grid()
-    plt.show()
-
-    # plotting the normal signal
-    # plotting.signal_plot(clean_signal[98000:])
-
-    # plotting the noisy signal
-    # plotting.signal_plot(noisy_signal[98000:])
-
-    # plotting the new signal!
-    # plotting.signal_plot(y[98000:])
-    # plotting.signal_plot(error)
+    # # print(len(results["MSE"]))
+    # plt.figure(figsize=(10, 6))
+    # x_axis = np.arange(0, len(results["MSE"]))
+    # plt.plot(x_axis, results["MSE"], c="darkviolet")
+    # plt.title("MSE")
+    # plt.grid()
+    # plt.show()
+    #
+    # plt.figure(figsize=(10, 6))
+    # x_axis = np.arange(0, len(results["SNR"]))
+    # plt.plot(x_axis, results["SNR"], c="crimson")
+    # plt.title("SNR")
+    # plt.grid()
+    # plt.show()
 
 
 if __name__ == "__main__":
--- adaptive_filter/utils/plotting.py
+++ adaptive_filter/utils/plotting.py
@@ -1,28 +1,119 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.typing import NDArray
 
 """
 PLOTTING UTILS FOR PRESENTING DIFFERNT ALGORITHMS
 """
 
 
-# plot to measure time domain of signal
-def signal_plot(signal):
-    """
-    Summary: Plot to trace a given signal in the time domain
+class PlotSuite:
+    def __init__(self, algorithm: str, num_samples: int) -> None:
+        """
+        Args:
+            algorithm (str): String description of the Plot being used
+            num_samples (int): Time in terms of number of samples
 
-    """
+        """
+        self.algorithm = algorithm
+        self.N = num_samples
 
-    # indexing x to get points over time
-    # x_time = np.arange(start=0, stop=signal.shape[0], step=fs)
-    # plt.plot(x_time, signal[x_time])
-    plt.plot(signal, color="darkviolet")
-    plt.grid(True)
-    plt.show()
+    # plot to measure time domain of signal
+    def signal_plot(self, signal: NDArray, description: str, subplot: bool = False):
+        """
+        Summary: Plot to trace a given signal in the time domain
 
+        Args:
+            signal (np.ndarray): Vector of output signal y_hat
+            description (str): String description of plot, used in title
 
-# plot to measure error changes over time
+        """
+        # indexing x to get points over time
+        plt.plot(signal, c="darkviolet", label=f"{self.algorithm}")
+        plt.title(f"{description} of {self.algorithm} algorithm")
+        plt.ylabel("Amplitude")  # rotate this
+        plt.xlabel(f"Time in {self.N} samples")
+        plt.legend()
+        plt.grid()
 
+        # if its not a subplot, show results
+        if subplot is False:
+            plt.show()
+        else:
+            return plt
 
-# plot to capture the weight changes over time
+    # plot to measure error changes over time
+    def error_plot(self, results: NDArray, error_metric: str, subplot: bool = False):
+        """
+        Summary: Plot to trace the error over time.
+
+        Args:
+            results (np.ndarray): Vector of error results
+            error_metric (str): String description of error_metric, used in title
+        """
+        x_axis = np.arange(0, len(results[error_metric]))
+        plt.title(f"{error_metric} of {self.algorithm}")
+        plt.plot(x_axis, results[error_metric], c="crimson", label=f"{error_metric}")
+        plt.title(error_metric)
+        plt.ylabel(error_metric)  # rotate this
+        plt.xlabel(f"Time in {self.N} samples")
+        plt.legend()
+        plt.grid()
+
+        # if its not a subplot, show results
+        if subplot is False:
+            plt.show()
+        else:
+            return plt
+
+    # plot to capture the weight changes over time
+    def weight_adjustments(self, weights: NDArray, time_k: int, subplot: bool = False):
+        """Summary: plots the adjustments to the weights over time k.
+
+        Args:
+            weights (np.ndarray): Vector of weights at time_k
+            time_k (int): Iteration number k.
+        """
+        pass
+
+        # # if its not a subplot, show results
+        # if subplot is False:
+        #     plt.show()
+        # else:
+        #     return plt
+
+    # plot to run all three and return them as subplots in one, if chosen
+    def full_plot_suite(
+        self,
+        signal: NDArray,
+        description: str,
+        results: NDArray,
+        error_metric: str,
+        weights: NDArray,
+        time_k: int,
+    ):
+        """Summary: Plots the entire suite of plots available as subplots.
+
+        Args:
+            signal (np.ndarray): Vector of output signal y_hat
+            description (str): String description of plot, used in title
+            results (np.ndarray): Vector of error results
+            error_metric (str): String description of error_metric, used in title
+            weights (np.ndarray): Vector of weights at time_k
+            time_k (int): Iteration number k.
+
+        Returns:
+            matplotlib.pyplot: Full plot containing each available plot as a subplot
+        """
+        # creating each individual plot as a subplot
+        plt.figure(figsize=(14, 8))
+        plt.title(f"Results of {self.algorithm} algorithm")
+        plt.subplot(3, 1, 1)
+        signal_plot = self.signal_plot(signal, description, subplot=True)
+        plt.subplot(3, 1, 2)
+        error_plot = self.error_plot(results, error_metric, subplot=True)
+
+        # show the full plot
+        plt.tight_layout()
+        plt.show()
